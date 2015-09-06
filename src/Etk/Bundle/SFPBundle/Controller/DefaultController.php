8270faf0f7fa90d80e0c056ec3e4dcbf62e9bc82
Mariano Eiberman <mariano.eiberman@gmail.com>
2015-09-05 22:04:52 -0300

    Se hac eun cambio apra revisar integridad del git

--- src/Etk/Bundle/SFPBundle/Controller/DefaultController.php
+++ src/Etk/Bundle/SFPBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        // @TODO ver bundle
         return $this->render('EtkSFPBundle:Default:index.html.twig', array('name' => $name));
     }
 }
